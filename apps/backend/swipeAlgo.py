7b8d3e5f085d9bbdfbdf5ed69b88fe31980e785a
pawnpug <popcorn.wennberg@gmail.com>
2026-04-17 16:36:23 +0200

    första iteration av swipe algon

--- apps/backend/swipeAlgo.py
+++ apps/backend/swipeAlgo.py
@@ -1,2 +1,42 @@
 # bestämmer vilka personer som visas för användaren, baserat på deras preferenser och tidigare swipes
+from user import User
 
+# skapa en lista med filtrerade användare som används för scoring
+def filter_users(current_user: User, all_users: list[User]) -> list[User]:
+    user_pool = []
+    for user in all_users:
+        age_ok = (user.age_range[0] <= current_user.age <= user.age_range[1] and current_user.age_range[0] <= user.age <= current_user.age_range[1])
+        gender_ok = (current_user.gender == user.gender)
+        not_blocked = (user.user_id not in current_user.blocked_users)
+        if age_ok and gender_ok and not_blocked:
+            user_pool.append(user)
+    return user_pool
+
+EVENT_MULTIPLIER = 80
+SONG_MOVIE_MULTIPLIER = 10
+ARTIST_DIRECTOR_MULTIPLIER = 7
+GENRE_MULTIPLIER = 5
+
+def scoring_users(current_user: User, user_pool: list[User]) -> list[tuple[User, int]]:
+    ranked_user_pool = []
+    for user in user_pool:
+        shared_events = len(set(current_user.events).intersection(set(user.events)))
+        shared_songs = len(set(current_user.songs).intersection(set(user.songs)))
+        shared_movies = len(set(current_user.movies).intersection(set(user.movies)))
+        shared_artists = len(set(current_user.artists).intersection(set(user.artists)))
+        shared_directors = len(set(current_user.directors).intersection(set(user.directors)))
+        shared_music_genre = len(set(current_user.music_genre).intersection(set(user.music_genre)))
+        shared_movie_genre = len(set(current_user.movie_genre).intersection(set(user.movie_genre)))
+        
+        event_score = shared_events * EVENT_MULTIPLIER
+        song_movie_score = (shared_songs + shared_movies) * SONG_MOVIE_MULTIPLIER
+        artist_director_score = (shared_artists + shared_directors) * ARTIST_DIRECTOR_MULTIPLIER
+        genre_score = (shared_music_genre + shared_movie_genre) * GENRE_MULTIPLIER
+        user_score = event_score + song_movie_score + artist_director_score + genre_score
+        ranked_user_pool.append((user, user_score))
+        
+    return sorted(ranked_user_pool, key=lambda x: x[1], reverse=True)
+
+def get_scored_users(current_user: User, all_users: list[User]) -> list[tuple[User, int]]:
+    user_pool = filter_users(current_user, all_users)
+    return scoring_users(current_user, user_pool)
