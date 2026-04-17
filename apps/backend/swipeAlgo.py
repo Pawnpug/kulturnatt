948752cc8ac0f3c124b52c63174d298f42790f9f
pawnpug <popcorn.wennberg@gmail.com>
2026-04-17 16:43:02 +0200

    la till comments

--- apps/backend/swipeAlgo.py
+++ apps/backend/swipeAlgo.py
@@ -12,13 +12,16 @@ def filter_users(current_user: User, all_users: list[User]) -> list[User]:
             user_pool.append(user)
     return user_pool
 
+# konstanter som används för scoring, kommer ändras...
 EVENT_MULTIPLIER = 80
 SONG_MOVIE_MULTIPLIER = 10
 ARTIST_DIRECTOR_MULTIPLIER = 7
 GENRE_MULTIPLIER = 5
 
+# använder de filtrerade användarna från user_pool för att räkna ut score
 def scoring_users(current_user: User, user_pool: list[User]) -> list[tuple[User, int]]:
     ranked_user_pool = []
+    #räkna hur många av varje typ de har gemensamt
     for user in user_pool:
         shared_events = len(set(current_user.events).intersection(set(user.events)))
         shared_songs = len(set(current_user.songs).intersection(set(user.songs)))
@@ -28,15 +31,17 @@ def scoring_users(current_user: User, user_pool: list[User]) -> list[tuple[User,
         shared_music_genre = len(set(current_user.music_genre).intersection(set(user.music_genre)))
         shared_movie_genre = len(set(current_user.movie_genre).intersection(set(user.movie_genre)))
         
+        # uträkning för score
         event_score = shared_events * EVENT_MULTIPLIER
         song_movie_score = (shared_songs + shared_movies) * SONG_MOVIE_MULTIPLIER
         artist_director_score = (shared_artists + shared_directors) * ARTIST_DIRECTOR_MULTIPLIER
         genre_score = (shared_music_genre + shared_movie_genre) * GENRE_MULTIPLIER
+        
         user_score = event_score + song_movie_score + artist_director_score + genre_score
         ranked_user_pool.append((user, user_score))
         
     return sorted(ranked_user_pool, key=lambda x: x[1], reverse=True)
-
+# huvudfunktion som kallar på de två övre, filter och scoring / sortering
 def get_scored_users(current_user: User, all_users: list[User]) -> list[tuple[User, int]]:
     user_pool = filter_users(current_user, all_users)
     return scoring_users(current_user, user_pool)
