b444cf5efce8eaea003d016a80e19063302260ac
HeavyHorndal <mcgooalbert@gmail.com>
2026-04-29 10:14:41 +0200

    Uppdaterade kod med sökningsfunktionalitet.

--- apps/backend/tmdb.py
+++ apps/backend/tmdb.py
@@ -6,6 +6,101 @@ load_dotenv()
 API_KEY = os.getenv('TMDB_API_KEY')
 BASE_URL = "https://api.themoviedb.org/3"
 
+########### SÖKNING EFTER DATA ###########
+
+def search_movie(query):
+    url = f"{BASE_URL}/search/movie"
+    params = {
+        "api_key": API_KEY,
+        "query": query
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    results = []
+
+    for movie in data.get("results", []):
+        results.append({
+            "id": movie.get("id"),
+            "title": movie.get("title"),
+            "year": movie.get("release_date", "")[:4],
+            "director": get_director(movie.get("id")),
+            "poster_path": build_poster_url(movie.get("poster_path"))
+        })
+    
+    return results
+
+def search_tv_show(query):
+    url = f"{BASE_URL}/search/tv"
+    params = {
+        "api_key": API_KEY,
+        "query": query
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    results = []
+
+    for show in data.get("results", []):
+        results.append({
+            "id": show.get("id"),
+            "name": show.get("name"),
+            "first aired": show.get("first_air_date"),
+            "poster_path": build_poster_url(show.get("poster_path"))
+        })
+    
+    return results
+
+def search_actor(query):
+    url = f"{BASE_URL}/search/person"
+    params = {
+        "api_key": API_KEY,
+        "query": query
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    results = []
+
+    for person in data.get("results", []):
+        if person.get("known_for_department") == "Acting":
+            results.append({
+                "id": person.get("id"),
+                "name": person.get("name"),
+                "date_of_birth": person.get("birthday"),
+                "profile_path": build_profile_url(person.get("profile_path"))
+            })
+    
+    return results
+
+def search_director(query):
+    url = f"{BASE_URL}/search/person"
+    params = {
+        "api_key": API_KEY,
+        "query": query
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    results = []
+
+    for person in data.get("results", []):
+        if person.get("known_for_department") == "Directing":
+            results.append({
+                "id": person.get("id"),
+                "name": person.get("name"),
+                "date_of_birth": person.get("birthday"),
+                "profile_path": build_profile_url(person.get("profile_path"))
+            })
+    
+    return results
+
+########### HÄMTNING AV DATA ###########
+
 def get_movie(movie_id):
     url = f"{BASE_URL}/movie/{movie_id}"
     params = {
@@ -78,16 +173,13 @@ def build_profile_url(path):                                    ## Sätter ihop 
         return f"https://image.tmdb.org/t/p/w300_and_h450_face{path}"
     return None
 
-movie = get_movie(24128)                                        ## Hämtar relevanta värden om en film baserat på angivet ID, i detta fall Stop Making Sense
-movie["director"] = get_director(24128)                         ## Hämtar namnet på regissör av film baserat på angivet ID, i detta fall Jonathan Demme
-movie["poster_url"] = build_poster_url(movie["poster_path"])    ## Bygger ihop en korrekt url för visning av primär affisch
+##movie = get_movie(24128)                                        ## Hämtar relevanta värden om en film baserat på angivet ID, i detta fall Stop Making Sense
+##movie["director"] = get_director(24128)                         ## Hämtar namnet på regissör av film baserat på angivet ID, i detta fall Jonathan Demme
+##movie["poster_url"] = build_poster_url(movie["poster_path"])    ## Bygger ihop en korrekt url för visning av primär affisch
 
-show = get_tv_show(30991)                                       ## Hämtar relevant data om en serie baserat på angivet ID, i detta fall Cowboy Bebop
-show["poster_url"] = build_poster_url(show["poster_path"])      ## Bygger ihop en korrekt url för visning av primär affisch
+##show = get_tv_show(30991)                                       ## Hämtar relevant data om en serie baserat på angivet ID, i detta fall Cowboy Bebop
+##show["poster_url"] = build_poster_url(show["poster_path"])      ## Bygger ihop en korrekt url för visning av primär affisch
 
-actor = get_person(5602)                                        ## Hämtar relevant data om en person baserat på angivet ID, i detta fall David Lynch
-actor["profile_path"] = build_profile_url(actor["profile_path"])
+##actor = get_person(5602)                                        ## Hämtar relevant data om en person baserat på angivet ID, i detta fall David Lynch
+##actor["profile_path"] = build_profile_url(actor["profile_path"])
 
-print(movie)
-print(show)
-print(actor)
